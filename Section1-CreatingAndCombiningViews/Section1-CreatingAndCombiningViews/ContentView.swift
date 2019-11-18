5f914624d7db2e00f3c3c4308d7cdf3ff1bc5586
Lvyuan <lvyuan@innotechx.com>
2019-11-18 17:19:52 +0800

    Creating And Combining Views

---
+++ Section1-CreatingAndCombiningViews/Section1-CreatingAndCombiningViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Section1-CreatingAndCombiningViews
+//
+//  Created by admin on 2019/11/18.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Swift!!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
